f86853abda26a217f0077ac203bae363b4141f68
brendan-ward <bcward@consbio.org>
2014-11-06 20:33:05 -0800

    Minor cleanup, made blank PNG data a global

--- main.go
+++ main.go
@@ -17,8 +17,10 @@ import (
 )
 
 var options struct {
-	Port     int    `long:"port" description:"Server port" default:"8080"`
-	Tilesets string `long:"tilesets" description:"path to tilesets" default:"./tilesets"`
+	Port      int    `long:"port" description:"Server port" default:"8080"`
+	Tilesets  string `long:"tilesets" description:"path to tilesets" default:"./tilesets"`
+	CachePort int    `long:"cacheport" description:"GroupCache port" default:"8000"`
+	CacheSize int64  `long:"cachesize" description:"Size of Cache (MB)" default:"10"`
 }
 
 var (
@@ -26,6 +28,7 @@ var (
 	cache       *groupcache.Group
 	connections map[string]*sql.DB
 	pngQueries  map[string]*sql.Stmt
+	blankPNG    []byte
 )
 
 func main() {
@@ -33,6 +36,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	blankPNG, _ = ioutil.ReadFile("blank.png")
 
 	connections = make(map[string]*sql.DB)
 	pngQueries = make(map[string]*sql.Stmt)
@@ -60,8 +64,8 @@ func main() {
 		pngQueries[service] = stmt
 	}
 
-	pool = groupcache.NewHTTPPool("http://127.0.0.1:8000")
-	cache = groupcache.NewGroup("TileCache", 64<<20, groupcache.GetterFunc(
+	pool = groupcache.NewHTTPPool(fmt.Sprintf("http://127.0.0.1:%v", options.CachePort))
+	cache = groupcache.NewGroup("TileCache", options.CacheSize*1048576, groupcache.GetterFunc(
 		func(ctx groupcache.Context, key string, dest groupcache.Sink) error {
 			// log.Println("Requested", key)
 
@@ -83,11 +87,7 @@ func main() {
 			var tile_data []byte
 			err := stmt.QueryRow(uint8(z), uint16(x), uint16(y)).Scan(&tile_data)
 			if err != nil {
-				if err == sql.ErrNoRows {
-					// log.Println("No tile found")
-					// tile_data = make([]byte, 1, 1)
-					// tile_data[0] = 0
-				} else {
+				if err != sql.ErrNoRows {
 					log.Fatal(err)
 				}
 			}
@@ -101,6 +101,9 @@ func main() {
 		var data []byte
 		pathParams := strings.Split(c.Params.ByName("key"), "/")
 		// fmt.Println("path segments", pathParams, len(pathParams))
+
+		// fmt.Println(cache.CacheStats(1))
+
 		if len(pathParams) != 5 {
 			c.Abort(400)
 			return
@@ -115,52 +118,12 @@ func main() {
 			return
 		}
 		if len(data) <= 1 {
-			// log.Println("Empty data")
-			data, _ = ioutil.ReadFile("blank.png")
+			data = blankPNG
 		}
 
-		// log.Println("Bytes returned:", len(data))
 		//TODO: make based on data
 		c.Data(200, "image/png", data)
 	})
-
-	// router.GET("/:service/:z/:x/:y", func(c *gin.Context) {
-	// 	service := c.Params.ByName("service")
-	// 	stmt, ok := pngQueries[service]
-	// 	if !ok {
-	// 		log.Printf("Service not found: %s", service)
-	// 		c.Abort(404)
-	// 		return
-	// 	}
-
-	// 	//TODO: cache response data in memory
-
-	// 	var tile_data []byte
-	// 	y_params := strings.Split(c.Params.ByName("y"), ".")
-	// 	if len(y_params) != 2 {
-	// 		c.Abort(400)
-	// 		return
-	// 	}
-
-	// 	z, _ := strconv.ParseUint(c.Params.ByName("z"), 0, 64)
-	// 	y, _ := strconv.ParseUint(y_params[0], 0, 64)
-	// 	extension := y_params[1]
-	// 	log.Println(extension)
-	// 	//flip y to match TMS spec
-	// 	y = (1 << z) - 1 - y
-
-	// 	err := stmt.QueryRow(z, c.Params.ByName("x"), y).Scan(&tile_data)
-	// 	if err != nil {
-	// 		if err == sql.ErrNoRows {
-	// 			tile_data, _ = ioutil.ReadFile("blank.png")
-	// 		} else {
-	// 			log.Fatal(err)
-	// 		}
-	// 	}
-	// 	//TODO set headers for cache control
-	// 	c.Writer.Header().Add("Cache-Control", "public, max-age= 300")
-	// 	c.Data(200, "image/png", tile_data)
-	// })
 
 	router.Run(fmt.Sprintf(":%v", options.Port))
 }
